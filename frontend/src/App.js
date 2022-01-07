f27e60403f4f58217c43c6ed1352deed0551645a
MikaKivennena <mika.kivennena@tuni.fi>
2022-01-07 23:16:03 +0200

    Create basic user interface to translate words. Fetch from SQL database

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,34 @@
 import './App.css';
 import Word from './Word';
 import {useState, useEffect} from 'react';
-import {useParams} from 'react-router-dom';
-
+import Score from './Score';
 function App() {
-  const url = "http://localhost:3010/vocabulary/1"
+  const currScor = 0;
+  const url = "http://localhost:3010/vocabulary"
   const[wordsArray, setWordsArray] = useState([]);
   useEffect(() => {
     fetch(url)
     .then(response => response.json())
-    .then(res => setWordsArray(res));
+    .then(res => setWordsArray(res))
+    .catch(err => console.log(err));
   },);
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <h1>
           Learn Language App
-        </p>
+        </h1>
       </header>
-      <div>
-        <h5>Welcome weary traveler</h5>
-
-        <Word word="Word" correctWord="sana"/>
-      </div>
-
-      <div>
-          <h1>{wordsArray.finnishWord}</h1>
-          <h1>{wordsArray.englishWord}</h1>
-          <h1>{wordsArray.category}</h1>
+      {wordsArray.map((word) => {
+      return (
+        <div>
+          <Word originalWord={word.englishWord} correctWord={word.finnishWord}/>
         </div>
+      )
+    })}
+
+    <Score currentScore={currScor}/>
     </div>
   );
 }
