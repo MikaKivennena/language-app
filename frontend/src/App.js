29c4fa224bdc0085b44a03d80e9bafcd671ee21b
MikaKivennena <mika.kivennena@tuni.fi>
2022-01-08 05:38:05 +0200

    Divide App.js to smaller components to create different views

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,9 @@
 import './App.css';
-import Word from './Word';
-import {useState, useEffect} from 'react';
-import Score from './Score';
-function App() {
-  const currScor = 0;
-  const url = "http://localhost:3010/vocabulary"
-  const[wordsArray, setWordsArray] = useState([]);
-  useEffect(() => {
-    fetch(url)
-    .then(response => response.json())
-    .then(res => setWordsArray(res))
-    .catch(err => console.log(err));
-  },);
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+import UserView from './UserView';
+import AdminView from './AdminView';
 
+function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,17 +11,29 @@ function App() {
           Learn Language App
         </h1>
       </header>
-      {wordsArray.map((word) => {
-      return (
-        <div>
-          <Word originalWord={word.englishWord} correctWord={word.finnishWord}/>
-        </div>
-      )
-    })}
-
-    <Score currentScore={currScor}/>
+      <BrowserRouter>
+    <div className="navigation">
+      <NavLink to="/main" className="link">
+        Main
+      </NavLink>
+      <NavLink to="/user" className="link">
+        User
+      </NavLink>
+      <NavLink to="/admin" className="link">
+        Admin
+      </NavLink>
     </div>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/user" element={<UserView />} />
+      <Route path="/admin" element={<AdminView />} />
+    </Routes>
+  </BrowserRouter>
+  </div>
   );
+
+
+
 }
 
 export default App;
