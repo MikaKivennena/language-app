af2331581b4198d7199843daf00e6405524373fc
MikaKivennena <mika.kivennena@tuni.fi>
2022-01-09 05:16:27 +0200

    Add basic funtionality to add new words to database

--- frontend/src/AdminView.js
+++ frontend/src/AdminView.js
@@ -1,11 +1,54 @@
+import { useState } from "react";
+
 function AdminView() {
 
+    const [englishWord, setEnglishWord] = useState('');
+    const [finnishWord, setFinnishWord] = useState('');
+    const [category, setCategory] = useState('');
 
-    return(
-        <div className="adminview">
-            <h1>Temporary admin view</h1>
-        </div>
-    )
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      const task = {englishWord, finnishWord, category };
+
+      fetch('http://localhost:3010/vocabulary', {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(task)
+      },window.location.reload(false))
+    }
+
+    return (
+      <div className="adminview">
+        <h2>Add a new word paird to database</h2>
+        <form onSubmit={handleSubmit}>
+          <label>english word</label><br/>
+          <input
+            type="text"
+            required
+            value={englishWord}
+            onChange={(e) => setEnglishWord(e.target.value)}
+          />
+          <br/>
+          <label>Finnish word</label><br/>
+          <input
+            type="text"
+            required
+            value={finnishWord}
+            onChange={(e) => setFinnishWord(e.target.value)}
+          />
+          <br/>
+          <label>category</label><br/>
+          <input
+            type="text"
+            required
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          />
+          <br/>
+          <button className="addButton" >Add to database</button>
+        </form>
+      </div>
+    );
 }
 
 export default AdminView;
