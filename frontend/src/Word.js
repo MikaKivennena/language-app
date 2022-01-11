b3b68972d0a3a7fef732f8bdc35f3822a56be179
Mika <mika.kivennena@gmail.com>
2022-01-11 23:28:13 +0200

    Add correct and false texts when user translates a word.

--- frontend/src/Word.js
+++ frontend/src/Word.js
@@ -1,26 +1,40 @@
 import {useState} from 'react';
-import Score from './Score';
-
+import './AdminView.css';
 const Word = (props) => {
     const[word, setWord] = useState('');
+    const[guessState, setGuessState] = useState("________");
+    const[nameClass, setClassName] = useState("");
     const checkWord = () => {
-        if(props.correctWord === word) {
-            console.log("Oikein!");
+        if(props.correctWord === word.toLowerCase()) {
+            setGuessState("Correct!")
+            setClassName("corr");
         } else {
-            console.log("Väärin!");
+            setGuessState("Wrong!");
+            setClassName("fals");
         }
     }
     return(
         <div>
-        <label>{props.originalWord} = </label>
+        <table>
+        <div>
+        <tr>
+        <th>
+        <label>{props.originalWord}</label>
+        </th>
+        <th>
         <input
         required
         type="text"
-        placeholder="Write here"
+        placeholder="Answer here"
         value={word}
         onChange={(e) => setWord(e.target.value)}
         />
-        <button onClick={checkWord}>check</button>
+        </th>
+            <th><button onClick={checkWord}>check</button></th>
+                <th className={nameClass}>{guessState}</th>
+        </tr>
+        </div>
+            </table>
         </div>
     );
 }
